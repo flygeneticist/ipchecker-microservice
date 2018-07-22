74d63d34b7f23a905818a8403414ce38c831aa44
Kevin Keller <flygeneticist@gmail.com>
2018-07-22 10:48:28 +0700

    basic request / responce handler setup

---
+++ index.handler.js
@@ -0,0 +1,36 @@
+console.log('Loading function');
+const doc = require('dynamodb-doc');
+const dynamo = new doc.DynamoDB();
+
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+            'X-hello-world': 'kk',
+        },
+    });
+
+    switch (event.httpMethod) {
+        case 'DELETE':
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+            break;
+        case 'GET':
+            var res = {
+                "ip_address": "${event.requestContext.identity.sourceIp}",
+            }
+            done("", res);
+            break;
+        case 'POST':
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+            break;
+        case 'PUT':
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+            break;
+        default:
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
+};
